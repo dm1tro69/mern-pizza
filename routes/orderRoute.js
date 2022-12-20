51bb2f20a491bd17d10a4b413c827c381c4d222d
Dmytro Voronov <dimolg22@gmail.com>
2022-12-20 14:38:18 +0200

    add stripe backend

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -1,10 +1,30 @@
 import {Router} from "express";
+import Stripe from 'stripe'
+const stripe = Stripe(process.env.STRIPE)
+import {v4 as uuidv4} from 'uuid'
 
 const router = Router()
 
 router.post('/placeorder', async (req, res) => {
+    const {token, subTotal, currentUser, cartItems} = req.body
     try {
-
+          const costumer = await stripe.customers.create({
+              email: token.email,
+              source: token.id
+          })
+        const payment = await stripe.charges.create({
+            amount: subTotal*100,
+            currency: 'USD',
+            customer: costumer.id,
+            receipt_email: token.email
+        }, {
+            idempotencyKey: uuidv4()
+        })
+        if (payment){
+            res.send('Payment Done')
+        } else {
+            res.send('Payment Fail')
+        }
     }catch (e) {
         res.json({message: e.message})
     }
